c31642536cfd37275562615f152be56ca238d545
PeterHsiao <u09800198@gmail.com>
2017-11-28 21:46:15 +0800

    data structure
    setChoose

--- Genetic/Solution.java
+++ Genetic/Solution.java
@@ -1,16 +1,29 @@
 package Genetic;
 
-import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Vector;
 
 import com.anselm.plm.utilobj.LogIt;
 
 public class Solution 
 {
+	/*
+	 * data structure
+	 * 
+	 * map: int[][]
+	 *   01........RB
+	 * 01[][][][][][]
+	 * 02[][][][][][]
+	 * .............
+	 * .............
+	 * BS[]........[]
+	 * 
+	 * choose: HashMap, key: UE, value: <BS,RB>
+	 */
 	final private int BSNumber = 9;
 	final private int RBNumber = 12;
 	
-	public ArrayList<Vector<Integer>> choose = new ArrayList<Vector<Integer>>();
+	public HashMap<Integer,Vector<Integer>> choose = new HashMap<Integer,Vector<Integer>>();
 	private int map[][];
 	private int number = 30;
 	
@@ -35,10 +48,9 @@ public class Solution
 				this.map[i][j] = -1;
 		for(int i = 0; i < number;)
 		{
-			int BSUsedNumber = 0;
 			int x = (int) (Math.random()*BSNumber);
 			int y = (int) (Math.random()*RBNumber);
-			if(this.map[x][y] == -1 || BSUsedNumber > -3)
+			if(this.map[x][y] == -1)
 			{
 				Vector<Integer> v = new Vector<Integer>(2);
 				int[] ch = new int[2];
@@ -47,7 +59,7 @@ public class Solution
 				v.add(x);
 				v.add(y);
 				this.map[x][y] = i;
-				this.choose.add(v);
+				//this.choose.put(i, v);
 				i++;
 			}
 		}
@@ -68,14 +80,55 @@ public class Solution
 	
 	public static void printChoose(Solution sol)
 	{
-		for(Vector<Integer> vec : sol.choose)
+		for(int i : sol.choose.keySet())
 		{
-			System.out.println(vec);
+			System.out.println(String.valueOf(i)+": "+sol.choose.get(i));
 		}
 	}
 	
+	public boolean setMap(int i, int j, int ue)
+	{
+		this.map[i][j] = ue;
+		return true;
+	}
 	
-	
+	public boolean setChoose()
+	{
+		//this.choose.clear();
+		HashMap<Integer,Vector<Integer>> temp = new HashMap<Integer,Vector<Integer>>();
+		int count = 0;
+		for(int i = 0; i < this.BSNumber; i++)
+		{
+			for(int j = 0; j < this.RBNumber; j++)
+			{
+				count+=this.map[i][j];
+				if(this.map[i][j] == -1)
+					continue;
+				
+				Vector<Integer> v = new Vector<Integer>(2);
+				if(temp.containsKey(this.map[i][j]))
+				{
+					System.out.print("debug");
+					return false;
+				}
+				else
+				{
+					v.add(i);v.add(j);
+					temp.put(this.map[i][j], v);
+				}
+			}
+		}
+		//check map is valid
+		if(count != (0+this.number-1)*this.number/2-this.BSNumber*this.RBNumber+this.number)
+		{
+			System.out.println(count);
+			System.out.println((0+this.number-1)*this.number/2-this.BSNumber*this.RBNumber+this.number);
+			return false;
+		}
+		this.choose.clear();
+		this.choose.putAll(temp);
+		return true;
+	}
 	public int[] getUserAssociation()
 	{
 		int x[] = null;
@@ -93,11 +146,46 @@ public class Solution
 		return x;
 	}
 	
-	public static Solution crossover(Solution sol, int index)
+	public Solution crossover(Solution sol, int index)
 	{
-		
+		Solution newsol = new Solution();
+		for(int i = 0; i < this.number; i++)
+		{
+			
+		}
 		
 		return null;
+	}
+	
+	public boolean mutation(int index)
+	{
+		for(int i = 0; i < this.BSNumber; i++)
+		{
+			for(int j = 0; j <this.RBNumber; j++)
+			{
+				if(this.map[i][j] == index)
+				{
+					this.map[i][j] = -1;
+					this.recover(index, i, j);
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+	
+	private void recover(int ue,int bs, int rb)
+	{
+		for(int i = 0; ; i+=90)
+		{
+			int offset = i/360;
+			double x = bs + Math.sin(Math.toRadians(i)) + offset;
+			double y = rb + Math.cos(Math.toRadians(i)) + offset;
+			if(this.map[(int)x][(int)y] == -1)
+			{
+				this.map[(int)x][(int)y] = ue;
+			}
+		}
 	}
 	
 	public static void main(String[] args)
@@ -106,6 +194,7 @@ public class Solution
 		
 		Solution sol = new Solution();
 		Solution.printMap(sol);
+		log.log(sol.setChoose());
 		Solution.printChoose(sol);
 //		for(int i : sol.getUserAssociation())
 //			log.log(i);
